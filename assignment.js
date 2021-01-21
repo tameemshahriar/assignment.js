fe9eb6dbe797a3306f9e923b31646bfa3c13484d
tameemshahriar <you@example.com>
2021-01-22 02:02:49 +0600

    third problem

--- assignment.js
+++ assignment.js
@@ -16,6 +16,30 @@ function budgetCalculator (watch,phone,laptop) {
 var result = budgetCalculator(2,4,5);
 console.log(result);
 
+//third Problem - hotelCost
+
+function hotelCost (Days) {
+
+    var totalBill = 0;
+
+    for (var i = 1; i < Days; i++) {   
+        if ( Days <= 10) {
+            var totalBill = 100 * Days;
+        }
+        else if ( Days >= 11 && Days <= 20 ) {
+            var totalBill = ((Days-10)*80 + 1000);
+        }
+        else {
+            var totalBill = ((Days-20)*50 + 1800);
+        }
+    }
+    return totalBill;   
+}
+
+var result = hotelCost(30);
+console.log(result);
+
+
 //fourth Problem - megaFriend
 function megaFriend(arr) {
     var lgth = 0;
