817b4a75f75667b0bf1a12885376c5fbb4a05f3a
tameemshahriar <you@example.com>
2021-01-22 12:54:53 +0600

    Error handling and comments done

--- assignment.js
+++ assignment.js
@@ -1,57 +1,89 @@
-//First Problem - kilometerToMeter 
+// https://github.com/tameemshahriar/assignment.js
 
-function kilometerToMeter (kilometer) {
-    var Meter = kilometer * 1000;
-    return Meter;
+// First Problem - kilometerToMeter
+
+function kilometerToMeter(kilometer) {
+    if (kilometer < 0) {
+        return "Please provide a positive value for kilometer."
+    }
+    else {
+        var Meter = kilometer * 1000; // 1km = 1000m
+        return Meter;
+    }
 }
 
-var result = kilometerToMeter(450);
-console.log(result);
+// First Problem - kilometerToMeter solved
 
-//Second Problem - kilometerToMeter 
-function budgetCalculator (watch,phone,laptop) {
-    var sum = watch * 50 + phone * 100 + laptop * 500;
-    return sum;
+
+// Second Problem - budgetCalculator
+
+function budgetCalculator(watch, phone, laptop) {
+    if (watch < 0 || phone < 0 || laptop < 0) {
+        return "Please provide a positive value for watch, phone and laptop. Items can not have a negetive value.";
+    }
+    else {
+        var totalBudget = watch * 50 + phone * 100 + laptop * 500; // Watch price : 50, Phone price : 100 , laptop price : 500
+        return totalBudget;
+    }
 }
-var result = budgetCalculator(2,4,5);
-console.log(result);
 
-//third Problem - hotelCost
+// Second Problem - budgetCalculator solved
 
-function hotelCost (Days) {
 
+// Third Problem - hotelCost 
+
+function hotelCost(Days) {
     var totalBill = 0;
-
-    for (var i = 1; i < Days; i++) {   
-        if ( Days <= 10) {
-            var totalBill = 100 * Days;
+    if (Days <= 0) {
+        return "Days can not be a negetive vaue.";
+    }
+    if (Days > 0) {
+        if (Days >= 1 && Days <= 10) {
+            var totalBill = 100 * Days; // Staying in for less then 10 days
         }
-        else if ( Days >= 11 && Days <= 20 ) {
-            var totalBill = ((Days-10)*80 + 1000);
+        else if (Days >= 11 && Days <= 20) {
+            var totalBill = ((Days - 10) * 80 + 1000); // Staying in between 11-20 days
         }
         else {
-            var totalBill = ((Days-20)*50 + 1800);
+            var totalBill = ((Days - 20) * 50 + 1800); //Staying in for more than 20 days
         }
     }
-    return totalBill;   
+    return totalBill;
 }
 
-var result = hotelCost(30);
-console.log(result);
+// Third Problem - hotelCost solved
 
 
-//fourth Problem - megaFriend
-function megaFriend(arr) {
-    var lgth = 0;
+// Fourth Problem - megaFriend 
+
+function megaFriend(Names) {
+    var stringLength = 0;
     var longest;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].length > lgth) {
-            var lgth = arr[i].length;
-            longest = arr[i];
+    if ( Names === undefined || Names.length == 0) {
+        return "Friend names empty or does not exist."
+    }
+    for (var i = 0; i < Names.length; i++) {
+        if (Names[i].length > stringLength) {  // check string length
+            var stringLength = Names[i].length; // assign string length
+            longest = Names[i]; // assign string name
         }
     }
     return longest;
 }
-var arr = ['Sazon','Nazmul','Opu', 'badhon', 'Rumon', 'Dipzolbhai'];
-var result = megaFriend(arr);
-console.log(result);
+
+// Fourth Problem - megaFriend solved
+
+// Calling all the functions and checking the output results
+
+var result = kilometerToMeter(5); // kilometerToMeter
+console.log(result); // kilometerToMeter
+
+var result = budgetCalculator(2, 6, 7); // budgetCalculator 
+console.log(result); // budgetCalculator 
+
+var result = hotelCost(11); //hotelCost
+console.log(result); //hotelCost
+
+var Names = ['Sazon','Nazmul','Opu', 'badhon', 'Rumon', 'Dipzolbhai', 'jalali kobutor']; // megaFriend 
+var result = megaFriend(Names); // megaFriend 
+console.log(result); // megaFriend 
